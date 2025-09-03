cf948c91a19d4d3353ca1070ec3e29dab92db86a
Pratik Giri <pratikgiri585@gmail.com>
2025-09-03 20:22:29 +0530

    Srtring class in java

--- String_in_java.java
+++ String_in_java.java
@@ -1,6 +1,7 @@
 public class String_in_java {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         System.out.println('a'+'b'); // add ASCII code
         System.out.println("a"+"b"); // string concatination
