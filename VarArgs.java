205dad2df3325093a5d1c279439c22deab63f074
Pratik Giri <pratikgiri585@gmail.com>
2025-09-07 22:50:01 +0530

    variable argument  in java

--- VarArgs.java
+++ VarArgs.java
@@ -9,7 +9,8 @@ public class VarArgs {
 
    static  int fun(int ...v){
         int sum=0;
-        for(int i=0;i<v.length;i++){            sum += v[i];
+        for(int i=0;i<v.length;i++){
+            sum += v[i];
         }
         return sum;
     }
