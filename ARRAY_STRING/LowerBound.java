8c55195c90e863af747691fd336699cc931b986c
Pratik Giri <pratikgiri585@gmail.com>
2025-10-03 12:17:48 +0530

    Lower bound finding

---
+++ ARRAY_STRING/LowerBound.java
@@ -0,0 +1,32 @@
+
+package ARRAY_STRING;
+//"Lower bounds --- smallest index such that  arr[index]>=target"
+public class LowerBound {
+
+    public static int lowerbound(int arr[],int target ,int n){
+        int low=0,high=n-1;
+        int ans=n;
+        while(low<=high){
+            int mid =(low+high)/2;
+
+            if(arr[mid]>=target){
+                high=mid-1;
+                ans=mid;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Lower bounds --- smallest index such that arr[index]>=target");
+        int arr[]={1,2,4,4,4,5,9,10,15,22};
+       int  n=arr.length,target=4 ;
+
+        System.out.println("Lower bound is "+lowerbound(arr,target,n));
+
+
+    }
+}
