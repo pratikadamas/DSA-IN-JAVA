9a488086846483df715c7954de2117c87e153db5
Pratik Giri <pratikgiri585@gmail.com>
2025-10-21 16:09:37 +0530

    the array contains duplicates element

---
+++ ArrayString/containsDuplicate.java
@@ -0,0 +1,21 @@
+package ArrayString;
+import java.util.*;
+
+public class containsDuplicate {
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i: nums){
+            if(!set.add(i))
+                return true; // duplicates
+        }
+        return false;// for unoque
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,2,3,4,5,4};
+        System.out.println("the Array contains duplicates -- "+containsDuplicate(nums));
+    }
+}
