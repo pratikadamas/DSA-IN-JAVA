d034c7a74d1ba1c7d36476c53457062dab62c5b5
Pratik Giri <pratikgiri585@gmail.com>
2025-11-19 20:32:08 +0530

    ArrayString/LongestConsecutiveSequence.java

--- ArrayString/LongestConsecutiveSequence.java
+++ ArrayString/LongestConsecutiveSequence.java
@@ -1,12 +1,37 @@
-package ARRAY_STRING;
+package ArrayString;
 
-import java.util.*;
+import java.util.HashSet;
 
 public class LongestConsecutiveSequence {
+    public static int longestConsecutive(int[] nums) {
+        if (nums.length == 0) return 0;
 
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+
+        int longest = 0;
+
+        for (int num : set) {
+            // Only start counting when `num - 1` does NOT exist (start of a sequence)
+            if (!set.contains(num - 1)) {
+                int current = num;
+                int streak = 1;
+
+                while (set.contains(current + 1)) {
+                    current++;
+                    streak++;
+                }
+
+                longest = Math.max(longest, streak);
+            }
+        }
+
+        return longest;
+    }
 
     public static void main(String[] args) {
-
-
+        int[] arr = {100, 4, 200, 1, 3, 2};
+        System.out.println("Longest consecutive sequence length: " + longestConsecutive(arr));
     }
 }
+
