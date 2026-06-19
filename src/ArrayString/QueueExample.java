4b2374872f5631fc99c8bca0ce428e41d62dfce1
Pratik Giri <pratikgiri585@gmail.com>
2026-06-19 19:01:23 +0530

    queue collection

--- src/ArrayString/QueueExample.java
+++ src/ArrayString/QueueExample.java
@@ -7,7 +7,6 @@ public class QueueExample {
         // ✅ Create a Queue (ArrayDeque is faster than LinkedList)
         Queue<Integer> queue = new ArrayDeque<>();
 
-
         // -------------------------------
         // 1️⃣ offer(E item) → Add element to the queue (end)
         // -------------------------------
