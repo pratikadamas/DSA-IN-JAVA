d9eec6d0c5d4b642aa6994564afad2db628f137f
Pratik Giri <pratikgiri585@gmail.com>
2026-07-04 00:16:35 +0530

    bfs traversal

--- src/Graph/BFS.java
+++ src/Graph/BFS.java
@@ -1,8 +1,5 @@
 package Graph;
-
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class BFS {
     public static void main(String[] args) {
