e75ccf623e4c6bf811ab334645ad901e74f598e4
Pratik Giri <pratikgiri585@gmail.com>
2026-07-31 23:38:35 +0530

    Triangular sum

--- src/ArrayString/TriangularSum.java
+++ src/ArrayString/TriangularSum.java
@@ -2,7 +2,8 @@ package ArrayString;
 
 import java.util.Arrays;
 
-public class TriangularSum {
+public class TriangularSum
+{
     public static void main(String[] args) {
         int arr[]={1};
 //        int arr[]={5,3,2,6,0};
