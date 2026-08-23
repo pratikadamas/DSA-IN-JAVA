490a82cb6b048b335144d61107fe9b3af9645ac7
Pratik Giri <pratikgiri585@gmail.com>
2026-08-23 23:48:44 +0530

    Kernighan Algorithm Count SetBitsinanInteger

--- src/BitManipulation/KernighanAlgorithmCountSetBitsinanInteger.java
+++ src/BitManipulation/KernighanAlgorithmCountSetBitsinanInteger.java
@@ -2,7 +2,8 @@ package BitManipulation;
 
 import java.util.Scanner;
 
-public class KernighanAlgorithmCountSetBitsinanInteger {
+public class KernighanAlgorithmCountSetBitsinanInteger
+{
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.println("Enter the number ");
